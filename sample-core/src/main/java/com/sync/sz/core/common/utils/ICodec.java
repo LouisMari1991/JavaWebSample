0573e85bdf70e6d486df3712817b51c79ad7f720
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-11-29 18:14:47 +0800

    init project

---
+++ sample-core/src/main/java/com/sync/sz/core/common/utils/ICodec.java
@@ -0,0 +1,26 @@
+package com.sync.sz.core.common.utils;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface ICodec {
+
+    ObjectMapper getMapper();
+
+    <T> T decode(byte[] json) throws JsonParseException, JsonMappingException, IOException;
+
+    <T> T decode(String json) throws JsonParseException, JsonMappingException, IOException;
+
+    <T> T decode(InputStream in) throws JsonParseException, JsonMappingException, IOException;
+
+    String encode(Object value) throws JsonProcessingException;
+
+    byte[] encodeByte(Object value) throws JsonProcessingException;
+
+
+}
