bd8015df2d3f32815b15476b73885ed74a79c2ab
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-12-03 14:02:51 +0800

    2016/12/03

---
+++ sample-service-platform/src/main/java/com/sync/sz/Main.java
@@ -0,0 +1,13 @@
+package com.sync.sz;
+
+import com.sync.sz.core.common.utils.JsonUtil;
+
+/**
+ * Created by Administrator on 2016/12/3 0003.
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+}
