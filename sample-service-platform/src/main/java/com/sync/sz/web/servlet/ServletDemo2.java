e5dd68e239927a92a8c90f502724fa80c8ae811b
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-12-05 22:34:50 +0800

    2016/12/05

---
+++ sample-service-platform/src/main/java/com/sync/sz/web/servlet/ServletDemo2.java
@@ -0,0 +1,28 @@
+package com.sync.sz.web.servlet;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Administrator on 2016/12/5 0005.
+ */
+public class ServletDemo2 extends HttpServlet {
+
+  @Override protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    super.doGet(req, resp);
+
+    Enumeration<String> enumeration = this.getServletConfig().getInitParameterNames();
+    while (enumeration.hasMoreElements()) {
+      System.out.println(this.getServletConfig().getInitParameter(enumeration.nextElement()));
+    }
+  }
+
+  @Override protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    super.doPost(req, resp);
+    this.doGet(req, resp);
+  }
+}
