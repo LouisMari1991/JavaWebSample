f9ef3138cf7d04bc887b21a723f1e966bfe0b446
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-12-21 17:33:00 +0800

    2016/12/21

---
+++ sample-netty/src/main/java/com/sync/sz/netty/nio/MultiplexerTimerServer.java
@@ -0,0 +1,89 @@
+package com.sync.sz.netty.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by YH on 2016-12-21.
+ */
+public class MultiplexerTimerServer implements Runnable {
+
+    private Selector selector;
+
+    private ServerSocketChannel servChannel;
+
+    private volatile boolean stop;
+
+    public MultiplexerTimerServer(int port) {
+        try {
+            selector = Selector.open();
+            servChannel = ServerSocketChannel.open();
+            servChannel.configureBlocking(false);
+            servChannel.bind(new InetSocketAddress(port), 1024);
+            servChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("The time server start in port : " + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void stop() {
+        this.stop = true;
+    }
+
+    @Override
+    public void run() {
+        while (!stop) {
+            try {
+                selector.select(100);
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> it = selectedKeys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()) {
+                    key = it.next();
+                    it.remove();
+                    try {
+                        handleInput(key);
+                    } catch (Exception e) {
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null) {
+                                key.channel().close();
+                            }
+                        }
+                    }
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
+        // 多路复用器关闭后，所有注册在上面的Channel和Pipe等资源都会被自动去注册并关闭，所以不需要重复释放资源。
+        if (selector != null) {
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleInput(SelectionKey key) throws IOException {
+        if (key.isValid()) {
+            // 处理新接入的请求消息
+            if (key.isAcceptable()) {
+                // Accept the new connection
+                ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+                SocketChannel sc = ssc.accept();
+                sc.configureBlocking(false);
+            }
+        }
+    }
+
+}
---
+++ sample-netty/src/main/java/com/sync/sz/netty/nio/TimerServer.java
@@ -0,0 +1,12 @@
+package com.sync.sz.netty.nio;
+
+/**
+ * Created by YH on 2016-12-21.
+ */
+public class TimerServer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
