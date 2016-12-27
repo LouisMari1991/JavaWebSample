aa5847bf4311577beb49eeb51274a90c8fc0addc
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-12-27 16:14:26 +0800

    2016/12/27

--- sample-netty/src/main/java/com/sync/sz/netty/frame/delimiter/EchoClient.java
+++ sample-netty/src/main/java/com/sync/sz/netty/frame/delimiter/EchoClient.java
@@ -1,7 +1,56 @@
 package com.sync.sz.netty.frame.delimiter;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
 /**
  * Created by YH on 2016-12-27.
  */
 public class EchoClient {
+
+  public void connect(int port, String host) throws Exception {
+    // 配置客户端NIO线程
+    EventLoopGroup group = new NioEventLoopGroup();
+    try {
+      Bootstrap b = new Bootstrap();
+      b.group(group).channel(NioSocketChannel.class)
+          .option(ChannelOption.TCP_NODELAY, true)
+          .handler(new ChannelInitializer<SocketChannel>() {
+            @Override protected void initChannel(SocketChannel ch) throws Exception {
+              ByteBuf delimiter = Unpooled.copiedBuffer("$_".getBytes());
+              ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, delimiter));
+              ch.pipeline().addLast(new StringDecoder());
+              ch.pipeline().addLast(new EchoClientHandler());
+            }
+          });
+      // 发起异步链接操作
+      ChannelFuture f = b.connect(host, port).sync();
+      // 等待客户端链路关闭
+      f.channel().closeFuture().sync();
+    } finally {
+      group.shutdownGracefully();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    int port = 8080;
+    if (args != null && args.length > 0) {
+      try {
+        port = Integer.valueOf(port);
+      } catch (NumberFormatException e) {
+        // 使用默认端口
+      }
+    }
+    new EchoClient().connect(port, "127.0.0.1");
+  }
 }
--- sample-netty/src/main/java/com/sync/sz/netty/frame/delimiter/EchoClientHandler.java
+++ sample-netty/src/main/java/com/sync/sz/netty/frame/delimiter/EchoClientHandler.java
@@ -1,7 +1,37 @@
 package com.sync.sz.netty.frame.delimiter;
 
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import java.util.logging.Logger;
+
 /**
  * Created by YH on 2016-12-27.
  */
-public class EchoClientHandler {
+public class EchoClientHandler extends ChannelHandlerAdapter {
+
+  private static final Logger logger = Logger.getLogger(EchoClientHandler.class.getName());
+
+  private int counter;
+
+  static final String ECHO_REQ = "Hi,XN. Welcome to Netty.$_";
+
+  public EchoClientHandler() {
+
+  }
+
+  @Override public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    for (int i = 0; i < 10; i++) {
+      ctx.writeAndFlush(Unpooled.copiedBuffer(ECHO_REQ.getBytes()));
+    }
+  }
+
+  @Override public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    System.out.printf("This is " + ++counter + " times receiver server : [" + msg + "]");
+  }
+
+  @Override public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    logger.warning(cause.toString());
+    ctx.close();
+  }
 }
