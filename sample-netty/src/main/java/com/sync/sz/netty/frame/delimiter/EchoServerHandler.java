4ddc4a72965078cdbd49e76c73ffeeb253416251
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-12-27 15:48:41 +0800

    2016/12/27

--- sample-netty/src/main/java/com/sync/sz/netty/frame/delimiter/EchoServerHandler.java
+++ sample-netty/src/main/java/com/sync/sz/netty/frame/delimiter/EchoServerHandler.java
@@ -1,7 +1,12 @@
 package com.sync.sz.netty.frame.delimiter;
 
+import io.netty.channel.ChannelHandlerAdapter;
+
 /**
  * Created by YH on 2016-12-27.
  */
-public class EchoServerHandler {
+public class EchoServerHandler extends ChannelHandlerAdapter {
+
+  
+
 }
