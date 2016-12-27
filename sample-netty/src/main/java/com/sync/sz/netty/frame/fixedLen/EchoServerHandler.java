29fe3c81e12176ac5fdfe840e5a54f8f2b54ce4d
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-12-27 16:45:08 +0800

    2016/12/27

--- sample-netty/src/main/java/com/sync/sz/netty/frame/fixedLen/EchoServerHandler.java
+++ sample-netty/src/main/java/com/sync/sz/netty/frame/fixedLen/EchoServerHandler.java
@@ -1,7 +1,22 @@
 package com.sync.sz.netty.frame.fixedLen;
 
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
 /**
  * Created by YH on 2016-12-27.
  */
-public class EchoServerHandler {
+public class EchoServerHandler extends ChannelHandlerAdapter {
+
+  private int counter;
+
+  @Override public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    System.out.println("this timer : " + counter + " ; Receiver client : [" + msg + "]");
+    ctx.writeAndFlush("Hello XN.");
+    counter++;
+  }
+
+  @Override public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    ctx.close();
+  }
 }
