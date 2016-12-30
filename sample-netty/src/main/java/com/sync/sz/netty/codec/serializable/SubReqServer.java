e721070d8d00a162ec2b11402820e657f76bbb99
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2016-12-30 18:48:49 +0800

    2016/12/30

---
+++ sample-netty/src/main/java/com/sync/sz/netty/codec/serializable/SubReqServer.java
@@ -0,0 +1,28 @@
+package com.sync.sz.netty.codec.serializable;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+
+/**
+ * Created by YH on 2016-12-30.
+ */
+public class SubReqServer {
+
+  public void bind(int port) throws Exception {
+    EventLoopGroup boosGroup = new NioEventLoopGroup();
+    EventLoopGroup workGroup = new NioEventLoopGroup();
+    try {
+      ServerBootstrap b = new ServerBootstrap();
+
+    } finally {
+      boosGroup.shutdownGracefully();
+      workGroup.shutdownGracefully();
+    }
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
