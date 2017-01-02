cd5e70925b234e8bb484625685735e91a3f749ef
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-02 12:48:00 +0800

    2017/1/2

---
+++ sample-netty/src/main/java/com/sync/sz/netty/codec/marshalling/SubReqServer.java
@@ -0,0 +1,7 @@
+package com.sync.sz.netty.codec.marshalling;
+
+/**
+ * Created by Administrator on 2017/1/2 0002.
+ */
+public class SubReqServer {
+}
---
+++ sample-netty/src/main/java/com/sync/sz/netty/codec/protobuf/SubReqServer.java
@@ -0,0 +1,7 @@
+package com.sync.sz.netty.codec.protobuf;
+
+/**
+ * Created by Administrator on 2017/1/2 0002.
+ */
+public class SubReqServer {
+}
