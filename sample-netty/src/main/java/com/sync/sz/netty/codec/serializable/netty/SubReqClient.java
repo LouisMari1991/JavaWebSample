f8e317926e909e508c9c03f6ee37f6059681ff99
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-02 12:10:10 +0800

    2017/1/2

--- sample-netty/src/main/java/com/sync/sz/netty/codec/serializable/netty/SubReqClient.java
+++ sample-netty/src/main/java/com/sync/sz/netty/codec/serializable/netty/SubReqClient.java
@@ -1,16 +1,53 @@
 package com.sync.sz.netty.codec.serializable.netty;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import org.omg.DynamicAny.DynUnionOperations;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolver;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
 
 /**
  * Created by Administrator on 2017/1/1 0001.
  */
 public class SubReqClient {
 
-  public void connect(int port, String host) {
+  public void connect(int port, String host) throws Exception {
     EventLoopGroup group = new NioEventLoopGroup();
+    try {
+      Bootstrap b = new Bootstrap();
+      b.group(group).channel(NioSocketChannel.class)
+          .option(ChannelOption.TCP_NODELAY, true)
+          .handler(new ChannelInitializer<SocketChannel>() {
+
+            @Override protected void initChannel(SocketChannel socketChannel) throws Exception {
+              socketChannel.pipeline().addLast(new ObjectDecoder(1024, ClassResolvers.cacheDisabled(this.getClass().getClassLoader())));
+              socketChannel.pipeline().addLast(new ObjectEncoder());
+              socketChannel.pipeline().addLast(new SubReqClientHandler());
+            }
+          });
+      ChannelFuture f = b.connect(host, port).sync();
+      f.channel().closeFuture().sync();
+    } finally {
+      group.shutdownGracefully();
+    }
   }
 
+  public static void main(String[] args) throws Exception {
+    int port = 8080;
+    if (args != null && args.length > 0) {
+      try {
+        port = Integer.valueOf(port);
+      } catch (NumberFormatException e) {
+
+      }
+    }
+    new SubReqClient().connect(port, "127.0.0.1");
+  }
 }
--- sample-netty/src/main/java/com/sync/sz/netty/codec/serializable/netty/SubReqClientHandler.java
+++ sample-netty/src/main/java/com/sync/sz/netty/codec/serializable/netty/SubReqClientHandler.java
@@ -1,7 +1,9 @@
 package com.sync.sz.netty.codec.serializable.netty;
 
+import io.netty.channel.ChannelHandlerAdapter;
+
 /**
  * Created by Administrator on 2017/1/1 0001.
  */
-public class SubReqClientHandler {
+public class SubReqClientHandler extends ChannelHandlerAdapter {
 }
