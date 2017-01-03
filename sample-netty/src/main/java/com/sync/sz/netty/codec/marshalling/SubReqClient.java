d2001c13eeab0b7cf6d84ae11ad29e08aa869edb
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-03 23:02:25 +0800

    2017/1/3

---
+++ sample-netty/src/main/java/com/sync/sz/netty/codec/marshalling/SubReqClient.java
@@ -0,0 +1,7 @@
+package com.sync.sz.netty.codec.marshalling;
+
+/**
+ * Created by Administrator on 2017/1/3 0003.
+ */
+public class SubReqClient {
+}
---
+++ sample-netty/src/main/java/com/sync/sz/netty/codec/marshalling/SubReqClientHandler.java
@@ -0,0 +1,16 @@
+package com.sync.sz.netty.codec.marshalling;
+
+import io.netty.channel.ChannelHandlerAdapter;
+
+/**
+ * Created by Administrator on 2017/1/3 0003.
+ */
+public class SubReqClientHandler extends ChannelHandlerAdapter {
+
+  public void connect(int port, String host) throws Exception {
+
+  }
+
+}
+
+
