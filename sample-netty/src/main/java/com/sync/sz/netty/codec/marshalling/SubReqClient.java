d1c1b282325fb0d7628d2165127bdeb8a11731d3
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-03 23:31:35 +0800

    2017/1/3

--- sample-netty/src/main/java/com/sync/sz/netty/codec/marshalling/SubReqClient.java
+++ sample-netty/src/main/java/com/sync/sz/netty/codec/marshalling/SubReqClient.java
@@ -1,12 +1,15 @@
 package com.sync.sz.netty.codec.marshalling;
 
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+
 /**
  * Created by Administrator on 2017/1/3 0003.
  */
 public class SubReqClient {
 
   public void connect(int port, String host) throws Exception {
-
+    EventLoopGroup group = new NioEventLoopGroup();
   }
 
 }
