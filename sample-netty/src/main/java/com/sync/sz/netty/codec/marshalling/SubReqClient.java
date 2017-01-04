51c42dade43d68b4e38abb24d44aefdd4d024144
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-04 16:59:10 +0800

    2017/01/04

--- sample-netty/src/main/java/com/sync/sz/netty/codec/marshalling/SubReqClient.java
+++ sample-netty/src/main/java/com/sync/sz/netty/codec/marshalling/SubReqClient.java
@@ -1,7 +1,13 @@
 package com.sync.sz.netty.codec.marshalling;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
 /**
  * Created by Administrator on 2017/1/3 0003.
@@ -10,6 +16,33 @@ public class SubReqClient {
 
   public void connect(int port, String host) throws Exception {
     EventLoopGroup group = new NioEventLoopGroup();
+    try {
+      Bootstrap b = new Bootstrap();
+      b.group(group).channel(NioSocketChannel.class)
+          .option(ChannelOption.TCP_NODELAY, true)
+          .handler(new ChannelInitializer<SocketChannel>() {
+            protected void initChannel(SocketChannel ch) throws Exception {
+              ch.pipeline().addLast(MarshallingCodeCFactory.buildMarshallingEncoder());
+              ch.pipeline().addLast(MarshallingCodeCFactory.buildMarshallingDecoder());
+              ch.pipeline().addLast(new SubReqClientHandler());
+            }
+          });
+      ChannelFuture f = b.connect(host, port).sync();
+      f.channel().closeFuture().sync();
+    } finally {
+      group.shutdownGracefully();
+    }
   }
 
+  public static void main(String[] args) throws Exception {
+    int port = 8080;
+    if (args != null && args.length > 0) {
+      try {
+        port = Integer.valueOf(port);
+      } catch (NumberFormatException e) {
+
+      }
+    }
+    new SubReqClient().connect(port, "127.0.0.1");
+  }
 }
--- sample-netty/src/main/java/com/sync/sz/netty/codec/marshalling/SubReqClientHandler.java
+++ sample-netty/src/main/java/com/sync/sz/netty/codec/marshalling/SubReqClientHandler.java
@@ -1,13 +1,46 @@
 package com.sync.sz.netty.codec.marshalling;
 
+import com.sync.sz.netty.codec.serializable.pojo.SubScribeReq;
 import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
 
 /**
  * Created by Administrator on 2017/1/3 0003.
  */
 public class SubReqClientHandler extends ChannelHandlerAdapter {
 
+  public SubReqClientHandler() {
 
+  }
+
+  @Override public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    for (int i = 0; i < 10; i++) {
+      ctx.write(subReq(i));
+    }
+    ctx.flush();
+  }
+
+  private SubScribeReq subReq(int i) {
+    SubScribeReq req = new SubScribeReq();
+    req.setAddress("深圳市南山区");
+    req.setProductName("Netty");
+    req.setSubReqID(i);
+    req.setUserName("SYNC");
+    return req;
+  }
+
+  @Override public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    System.out.println("Receiver server response : [" + msg + "]");
+  }
+
+  @Override public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+    ctx.flush();
+  }
+
+  @Override public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    cause.printStackTrace();
+    ctx.close();
+  }
 
 }
 
