53bedb91bd791e13d87267bc9cfb4af9576efe41
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-04 21:15:23 +0800

    2017/1/4

--- sample-netty/src/main/java/com/sync/sz/netty/protocol/http/fileServer/HttpFileServer.java
+++ sample-netty/src/main/java/com/sync/sz/netty/protocol/http/fileServer/HttpFileServer.java
@@ -7,6 +7,10 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
 import java.net.InetAddress;
 
 /**
@@ -25,12 +29,18 @@ public class HttpFileServer {
           .channel(NioServerSocketChannel.class)
           .childHandler(new ChannelInitializer<SocketChannel>() {
             @Override protected void initChannel(SocketChannel ch) throws Exception {
-
+              ch.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+              ch.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65536));
+              ch.pipeline().addLast("http-encoder", new HttpResponseEncoder());
+              ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+              ch.pipeline().addLast("fileServerHandler", new HttpFileServerHandler(url));
             }
           });
       String ip = InetAddress.getLocalHost().getHostAddress();
-      ChannelFuture f = b.bind(ip, port);
+      ChannelFuture f = b.bind(ip, port).sync();
       System.out.println("HTTP文件目录服务器启动，网址是 : http://" + ip + ":" + port + url);
+      f.channel().closeFuture().sync();
+      System.out.println("HTTP文件目录服务器关闭");
     } finally {
       bossGroup.shutdownGracefully();
       workerGroup.shutdownGracefully();
--- sample-netty/src/main/java/com/sync/sz/netty/protocol/http/fileServer/HttpFileServerHandler.java
+++ sample-netty/src/main/java/com/sync/sz/netty/protocol/http/fileServer/HttpFileServerHandler.java
@@ -1,8 +1,19 @@
 package com.sync.sz.netty.protocol.http.fileServer;
 
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.util.CharsetUtil;
+import java.io.File;
+import javax.activation.MimetypesFileTypeMap;
 
 /**
  * Created by YH on 2017-01-04.
@@ -16,6 +27,34 @@ public class HttpFileServerHandler extends SimpleChannelInboundHandler<FullHttpR
   }
 
   @Override protected void messageReceived(ChannelHandlerContext ctx, FullHttpRequest msg) throws Exception {
+    if (!msg.getDecoderResult().isSuccess()) {
 
+    }
+  }
+
+  private static void sendListing(ChannelHandlerContext ctx, File dir) {
+    FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+    response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/html; charset=UTF-8");
+    StringBuilder buf = new StringBuilder();
+    String dirPath = dir.getPath();
+    //buf.append()
+  }
+
+  private static void sendRedirect(ChannelHandlerContext ctx, String newUri) {
+    FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.FOUND);
+    response.headers().set(HttpHeaders.Names.LOCATION, newUri);
+    ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+  }
+
+  private static void sendError(ChannelHandlerContext ctx, HttpResponseStatus status) {
+    FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, status,
+        Unpooled.copiedBuffer("Failure: " + status.toString() + "\r\n", CharsetUtil.UTF_8));
+    response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=UTF-8");
+    ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+  }
+
+  private static void sendContentTypeHeader(HttpResponse response, File file) {
+    MimetypesFileTypeMap mimeTypesMap = new MimetypesFileTypeMap();
+    response.headers().set(HttpHeaders.Names.CONTENT_TYPE, mimeTypesMap.getContentType(file.getPath()));
   }
 }
