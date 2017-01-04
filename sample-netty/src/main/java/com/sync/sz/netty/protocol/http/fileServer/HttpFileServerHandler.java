2a98debd2753ff89d7bc36c4594ccc18904b6322
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-04 21:56:22 +0800

    2017/1/4

--- sample-netty/src/main/java/com/sync/sz/netty/protocol/http/fileServer/HttpFileServerHandler.java
+++ sample-netty/src/main/java/com/sync/sz/netty/protocol/http/fileServer/HttpFileServerHandler.java
@@ -1,5 +1,6 @@
 package com.sync.sz.netty.protocol.http.fileServer;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
@@ -13,6 +14,9 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.util.CharsetUtil;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.regex.Pattern;
 import javax.activation.MimetypesFileTypeMap;
 
 /**
@@ -32,12 +36,75 @@ public class HttpFileServerHandler extends SimpleChannelInboundHandler<FullHttpR
     }
   }
 
+  @Override public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    cause.printStackTrace();
+    if (ctx.channel().isActive()) {
+      sendError(ctx, HttpResponseStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  private static final Pattern INSECURE_URI = Pattern.compile(".*[<>&\"].*");
+
+  private String sanitizeUri(String uri) {
+    try {
+      uri = URLDecoder.decode(uri, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      try {
+        uri = URLDecoder.decode(uri, "ISO-8859-1");
+      } catch (UnsupportedEncodingException e1) {
+        throw new Error();
+      }
+    }
+    if (!uri.startsWith(url)) {
+      return null;
+    }
+    if (!uri.startsWith("/")) {
+      return null;
+    }
+    uri = uri.replace('/', File.separatorChar);
+    if (uri.contains(File.separator + ".") || uri.startsWith(".")
+        || uri.endsWith(".") || INSECURE_URI.matcher(uri).matches()) {
+      return null;
+    }
+    return System.getProperty("user.dir") + File.separator + uri;
+  }
+
+  private static final Pattern ALLOWED_FILE_NAME = Pattern.compile("[A-Za-z0-9][-_A-Za-z0-9\\\\.]*");
+
   private static void sendListing(ChannelHandlerContext ctx, File dir) {
     FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
     response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/html; charset=UTF-8");
     StringBuilder buf = new StringBuilder();
     String dirPath = dir.getPath();
-    //buf.append()
+    buf.append("<!DOCTYPE html>\r\n");
+    buf.append("<html><head><title>");
+    buf.append(dirPath);
+    buf.append(" 目录: ");
+    buf.append("</title></head><body>\r\n");
+    buf.append("<h3>");
+    buf.append(dirPath).append(" 目录: ");
+    buf.append("</h3>\r\n");
+    buf.append("<ul>");
+    buf.append("<li>链接：<a href=\"../\">..</a><li>\r\n");
+    for (File f : dir.listFiles()) {
+      if (f.isHidden() || !f.canRead()) {
+        continue;
+      }
+      String name = f.getName();
+      if (!ALLOWED_FILE_NAME.matcher(name).matches()) {
+        continue;
+      }
+      buf.append("<li>链接：<a href=\"");
+      buf.append(name);
+      buf.append("\">");
+      buf.append(name);
+      buf.append("</a></li>\r\n");
+    }
+    buf.append("</ul></body></html>\r\n");
+    ByteBuf buffer = Unpooled.copiedBuffer(buf, CharsetUtil.UTF_8);
+    response.content().writeBytes(buffer);
+    buffer.release();
+    ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
   }
 
   private static void sendRedirect(ChannelHandlerContext ctx, String newUri) {
