b8d0dd3d96f35911e77f17bf904d39ec0dc0803a
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-05 09:40:37 +0800

    2017/01/05

---
+++ sample-netty/src/test/java/com/sync/sz/netty/protocol/http/fileServer/HttpFileServerHandlerTest.java
@@ -0,0 +1,21 @@
+package com.sync.sz.netty.protocol.http.fileServer;
+
+import java.io.File;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by YH on 2017-01-05.
+ */
+public class HttpFileServerHandlerTest {
+
+  @Test
+  public void test1() {
+    System.out.println(System.getProperty("user.dir"));
+    System.out.println(File.separator);
+    System.out.println(File.separatorChar);
+  }
+
+
+}
