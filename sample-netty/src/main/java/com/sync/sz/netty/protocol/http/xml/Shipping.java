1bf4d20c09040ba6c22958e45e0df6f63ed5b05c
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-06 10:55:35 +0800

    2017/01/06

--- sample-netty/src/main/java/com/sync/sz/netty/protocol/http/xml/Shipping.java
+++
@@ -1,8 +0,0 @@
-package com.sync.sz.netty.protocol.http.xml;
-
-/**
- * Created by YH on 2017-01-05.
- */
-public enum Shipping {
-  STANDARD_MAIL, PRIORITY_MAIL, INTERNATIONAL_MAIL, DOMESTIC_EXPRESS, INTERNATIONAL_EXPRESS
-}
