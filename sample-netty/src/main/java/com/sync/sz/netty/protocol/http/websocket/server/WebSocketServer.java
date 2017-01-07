d81571800c7a71a25b5ed02ced98198ac1cfc50a
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-07 21:51:53 +0800

    2017/1/7

---
+++ sample-netty/src/main/java/com/sync/sz/netty/protocol/http/websocket/server/WebSocketServer.java
@@ -0,0 +1,45 @@
+package com.sync.sz.netty.protocol.http.websocket.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * Created by Administrator on 2017/1/7 0007.
+ */
+public class WebSocketServer {
+  public void run(int port) throws Exception {
+    EventLoopGroup bossGroup = new NioEventLoopGroup();
+    EventLoopGroup workerGroup = new NioEventLoopGroup();
+    try {
+      ServerBootstrap b = new ServerBootstrap();
+      b.group(bossGroup, workerGroup)
+          .channel(NioServerSocketChannel.class)
+          .childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override protected void initChannel(SocketChannel ch) throws Exception {
+              ChannelPipeline pipeline = ch.pipeline();
+              pipeline.addLast("http-codec", new HttpServerCodec());
+              pipeline.addLast("aggregator", new HttpObjectAggregator(65536));
+              ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+              //pipeline.addLast("handler",
+              //    new WebSocketServerHandler());
+            }
+          });
+      Channel ch = b.bind(port).sync().channel();
+      System.out.println("Web socket server start at port : " + port + '.');
+      ch.closeFuture().sync();
+    } finally {
+      bossGroup.shutdownGracefully();
+      workerGroup.shutdownGracefully();
+    }
+  }
+}
---
+++ sample-netty/src/main/java/com/sync/sz/netty/protocol/http/websocket/server/WebSocketServerHandler.java
@@ -0,0 +1,7 @@
+package com.sync.sz.netty.protocol.http.websocket.server;
+
+/**
+ * Created by Administrator on 2017/1/7 0007.
+ */
+public class WebSocketServerHandler {
+}
