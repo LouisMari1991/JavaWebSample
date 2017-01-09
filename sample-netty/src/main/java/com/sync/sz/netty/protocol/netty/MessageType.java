2398f12f71c9b5bf4c59ec5cfafcac2cd47842c7
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-09 22:20:33 +0800

    2017/1/9

--- sample-netty/src/main/java/com/sync/sz/netty/protocol/netty/MessageType.java
+++ sample-netty/src/main/java/com/sync/sz/netty/protocol/netty/MessageType.java
@@ -3,5 +3,24 @@ package com.sync.sz.netty.protocol.netty;
 /**
  * Created by Administrator on 2017/1/9 0009.
  */
-public class MessageType {
+public enum MessageType {
+
+  SERVICE_REQ((byte) 0),
+  SERVER_RESP((byte) 1),
+  ONE_WAY((byte) 2),
+  LOGIN_REQ((byte) 3),
+  LOGIN_RESP((byte) 4),
+  HEARTBEAT_REQ((byte) 5),
+  HEARTBEAT_RESP((byte) 6);
+
+  private byte value;
+
+  private MessageType(byte value) {
+    this.value = value;
+  }
+
+  public byte value() {
+    return this.value;
+  }
+
 }
--- sample-netty/src/main/java/com/sync/sz/netty/protocol/netty/NettyConstant.java
+++ sample-netty/src/main/java/com/sync/sz/netty/protocol/netty/NettyConstant.java
@@ -4,4 +4,8 @@ package com.sync.sz.netty.protocol.netty;
  * Created by Administrator on 2017/1/9 0009.
  */
 public class NettyConstant {
+  public static final String REMOTEIP = "127.0.0.1";
+  public static final int PORT = 8080;
+  public static final int LOCAL_PORT = 12088;
+  public static String LOCALIP = "127.0.0.1";
 }
