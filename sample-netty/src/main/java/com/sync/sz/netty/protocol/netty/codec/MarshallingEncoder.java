10a935181609fd42597f75c19dcd9c2546dc2c14
“youMarishunxiang@gmail.com <mousefly100%>
2017-01-11 22:18:19 +0800

    2017/1/11

--- sample-netty/src/main/java/com/sync/sz/netty/protocol/netty/codec/MarshallingEncoder.java
+++ sample-netty/src/main/java/com/sync/sz/netty/protocol/netty/codec/MarshallingEncoder.java
@@ -1,15 +1,32 @@
 package com.sync.sz.netty.protocol.netty.codec;
 
-import java.io.IOException;
-import org.jboss.marshalling.Unmarshaller;
+import io.netty.buffer.ByteBuf;
+import org.jboss.marshalling.Marshaller;
 
 /**
  * Created by Administrator on 2017/1/11 0011.
  */
 public class MarshallingEncoder {
-  private final Unmarshaller unmarshaller;
+
+  private static final byte[] LENGTH_PLACEHOLDER = new byte[4];
+  private final Marshaller marshaller;
 
   public MarshallingEncoder() throws Exception {
-    this.unmarshaller = MarshallingCodecFactory.buildUnmarshalling();
+    this.marshaller = MarshallingCodecFactory.buildMarshalling();
+  }
+
+  protected Object encode(Object msg, ByteBuf out) throws Exception {
+    try {
+      int lengthPos = out.writerIndex();
+      out.writeBytes(LENGTH_PLACEHOLDER);
+      ChannelBufferByteOutput output = new ChannelBufferByteOutput(out);
+      marshaller.start(output);
+      marshaller.writeObject(msg);
+      marshaller.finish();
+      out.setInt(lengthPos, out.writerIndex() - lengthPos - 4);
+    } finally {
+      marshaller.close();
+    }
+    return null;
   }
 }
