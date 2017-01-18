eccb3e4d44e1e973f14d4ada7c25f2a1f29cdd63
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-18 19:04:14 +0800

    2017/01/18

--- sample-netty/src/main/java/com/sync/sz/netty/protocol/netty/server/NettyServer.java
+++ sample-netty/src/main/java/com/sync/sz/netty/protocol/netty/server/NettyServer.java
@@ -1,10 +1,39 @@
 package com.sync.sz.netty.protocol.netty.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 /**
  * Created by Administrator on 2017/1/9 0009.
  */
 public class NettyServer {
-  public static void main(String[] args) {
 
+  public void run(int port) throws Exception {
+    EventLoopGroup bossGroup = new NioEventLoopGroup();
+    EventLoopGroup workerGroup = new NioEventLoopGroup();
+    try {
+      ServerBootstrap b = new ServerBootstrap();
+      b.group(bossGroup, workerGroup)
+          .channel(NioServerSocketChannel.class);
+      ChannelFuture f = b.bind(port).sync();
+      f.channel().closeFuture().sync();
+    } finally {
+      bossGroup.shutdownGracefully();
+      workerGroup.shutdownGracefully();
+    }
+  }
+
+  public static void main(String[] args) {
+    int port = 8080;
+    if (args != null && args.length > 0) {
+      try {
+        port = Integer.valueOf(args[0]);
+      } catch (NumberFormatException e) {
+
+      }
+    }
   }
 }
