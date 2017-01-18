fd5a78108ded4432812599bbd5be061b7dae11cc
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-01-18 18:23:40 +0800

    2017/01/18

--- sample-netty/src/main/java/com/sync/sz/netty/protocol/netty/client/NettyClient.java
+++ sample-netty/src/main/java/com/sync/sz/netty/protocol/netty/client/NettyClient.java
@@ -1,7 +1,18 @@
 package com.sync.sz.netty.protocol.netty.client;
 
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+
 /**
  * Created by Administrator on 2017/1/9 0009.
  */
 public class NettyClient {
+
+  private void connect(int port) throws Exception {
+    EventLoopGroup group = new NioEventLoopGroup();
+  }
+
+  public static void main(String[] args) {
+    int port = 8080;
+  }
 }
--- sample-netty/src/main/java/com/sync/sz/netty/protocol/netty/server/NettyServer.java
+++ sample-netty/src/main/java/com/sync/sz/netty/protocol/netty/server/NettyServer.java
@@ -4,4 +4,7 @@ package com.sync.sz.netty.protocol.netty.server;
  * Created by Administrator on 2017/1/9 0009.
  */
 public class NettyServer {
+  public static void main(String[] args) {
+
+  }
 }
