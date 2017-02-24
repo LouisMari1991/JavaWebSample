d9d65491b556d2161547bb08b41d6685545b0679
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-02-24 22:18:31 +0800

    2017/2/24

---
+++ sample-servlet/src/main/java/com/sync/sz/sample/dao/impl/UserDaoImpl.java
@@ -0,0 +1,27 @@
+package com.sync.sz.sample.dao.impl;
+
+import com.sync.sz.sample.domain.User;
+
+/**
+ * Created by Administrator on 2017/2/24 0024.
+ */
+public class UserDaoImpl {
+
+  public void add(User user) {
+
+  }
+
+  /**
+   * 登录
+   */
+  public User find(String username, String password) {
+    return null;
+  }
+
+  /**
+   * 用户是否存在
+   */
+  public User find(String username) {
+    return null;
+  }
+}
---
+++ sample-servlet/src/main/java/com/sync/sz/sample/utils/XmlUtils.java
@@ -0,0 +1,17 @@
+package com.sync.sz.sample.utils;
+
+import org.dom4j.Document;
+
+/**
+ * Created by Administrator on 2017/2/24 0024.
+ */
+public class XmlUtils {
+
+  public static Document getDocument() {
+    return null;
+  }
+
+  public static void write2Xml(Document document) {
+
+  }
+}
