c22ecc79e2c4c0fb2ddbc229a844adc1f5d52e13
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-02-28 16:54:14 +0800

    2017/02/28

---
+++ sample-hibernate/src/test/java/com/sync/sz/hibernate/entity/UserTest.java
@@ -0,0 +1,50 @@
+package com.sync.sz.hibernate.entity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by YH on 2017-02-28.
+ */
+public class UserTest {
+  @Test
+  public void getUid() throws Exception {
+
+  }
+
+  @Test
+  public void setUid() throws Exception {
+
+  }
+
+  @Test
+  public void getUsername() throws Exception {
+
+  }
+
+  @Test
+  public void setUsername() throws Exception {
+
+  }
+
+  @Test
+  public void getPassword() throws Exception {
+
+  }
+
+  @Test
+  public void setPassword() throws Exception {
+
+  }
+
+  @Test
+  public void getAddress() throws Exception {
+
+  }
+
+  @Test
+  public void setAddress() throws Exception {
+
+  }
+}
