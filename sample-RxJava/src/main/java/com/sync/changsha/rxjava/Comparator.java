bfe8ab09e0bff054135b94a4730198ec8f810abf
Marishunxiang@gmail.com <marishunxiang@gmail.com>
2017-07-07 11:45:12 +0800

    add module RxJava

---
+++ sample-RxJava/src/main/java/com/sync/changsha/rxjava/Comparator.java
@@ -0,0 +1,69 @@
+package com.sync.changsha.rxjava;
+
+import org.junit.Test;
+import rx.Observable;
+import rx.Observer;
+import rx.functions.Func1;
+import rx.functions.Func2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
+
+/**
+ * <p>
+ * 排序测试
+ * </p>
+ * Created by Administrator on 2017/7/7 0007.
+ */
+public class Comparator {
+
+    @Test
+    public void test() {
+
+        List<Integer> list = new ArrayList<>();
+
+        Random random = new Random();
+
+        for (int i = 0; i < 20; i++) {
+            list.add(random.nextInt(100));
+        }
+        System.out.println(list);
+        Observable.from(list)
+                .filter(new Func1<Integer, Boolean>() {
+                    @Override
+                    public Boolean call(Integer integer) {
+                        return integer.intValue() > 5;
+                    }
+                }).toSortedList(new Func2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer, Integer integer2) {
+                if (integer > integer2) {
+                    return -1;
+                } else if (Objects.equals(integer, integer2)) {
+                    return 0;
+                }
+                return 1;
+            }
+        }).subscribe(new Observer<List<Integer>>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onNext(List<Integer> integers) {
+                System.out.println(integers);
+            }
+        });
+
+    }
+
+}
